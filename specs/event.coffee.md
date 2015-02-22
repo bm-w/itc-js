5d160d56e7468f851c38845f59ec8782ca919690
Bastiaan Marinus van de Weerd <id@bm-w.eu>
2015-02-22 11:20:18 -0300

    Write `Event.join` class method, and tests.

--- specs/event.coffee.md
+++ specs/event.coffee.md
@@ -25,3 +25,27 @@
     		(expect ev).to.be.an.instanceOf @Event
     		(expect ev).to.have.a.property 'tree', 0
     		(expect ev).to.equalEvent 0
+
+    	it "should have a `join` class method", ->
+    		(expect @Event).to.have.a.property 'join'
+    			.that.is.a 'function'
+
+    	describe "its `join` class method", ->
+    		it "should join EV0 and EV1 instances into an EV1 instance", ->
+    			ev = @Event.join (new @Event 0), (new @Event 1)
+    			(expect ev, "the EV0-EV1 join").to.equalEvent 1
+
+    		it "should join EV0 and EV[1,0,1] instances into an EV[1,0,1] instance", ->
+    			ev = @Event.join (new @Event 0), (new @Event [1, 0, 1])
+    			(expect ev, "the EV0-EV[1,0,1] join").to.equalEvent [1, 0, 1]
+
+    			ev = @Event.join (new @Event [1, 0, 1]), (new @Event 0)
+    			(expect ev, "the EV[1,0,1]-EV0 join").to.equalEvent [1, 0, 1]
+
+    		it "should join EV[2,2,0] and EV[3,0,1] instances into an EV4 instance", ->
+    			ev = @Event.join (new @Event [2, 2, 0]), (new @Event [3, 0, 1])
+    			(expect ev, "the EV[2,2,0]-EV[3,0,1] join").to.equalEvent 4
+
+    		it "should join arbitrary instances into an instance", ->
+    			ev = @Event.join (new @Event [2, [2, 1, 0], 0]), (new @Event [2, 0, 3])
+    			(expect ev, "the EV[2,[2,1,0],0]-EV[2,0,3] join").to.equalEvent [4, [0, 1, 0,], 1]
--- src/event.js
+++ src/event.js
@@ -10,6 +10,87 @@ function ITCEvent(tree) {
 };
 
 
+function liftITCEventTree(tree, delta) {
+	if (typeof tree == 'number') { return tree + delta; }
+	else { return [tree[0] + delta, tree[1], tree[2]]; };
+};
+
+function sinkITCEventTree(tree, delta) {
+	if (typeof tree == 'number') { return tree - delta; }
+	else { return [tree[0] - delta, tree[1], tree[2]]; };
+};
+
+function maxITCEventTree(tree) {
+	if (typeof tree == 'number') { return tree; }
+	else { return tree[0] + Math.max(
+		maxITCEventTree.call(this, tree[1]),
+		maxITCEventTree.call(this, tree[2])
+	); };
+};
+
+function minITCEventTree(tree) {
+	if (typeof tree == 'number') { return tree; }
+	else { return tree[0] + Math.min(
+		minITCEventTree.call(this, tree[1]),
+		minITCEventTree.call(this, tree[2])
+	); };
+};
+
+function normITCEventTree(tree) {
+	var tree1 = undefined;
+
+	if (typeof tree == 'number') {
+		return tree;
+	} else if (typeof (tree1 = tree[1]) == 'number' && tree[1] === tree[2]) {
+		return tree[0] + tree1;
+	} else {
+		var tree1 = normITCEventTree.call(this, tree[1]),
+		    tree2 = normITCEventTree.call(this, tree[2]);
+		var sinkDelta = Math.min(
+			minITCEventTree.call(this, tree1),
+			minITCEventTree.call(this, tree2)
+		);
+
+		return [
+			tree[0] + sinkDelta,
+			sinkITCEventTree.call(this, tree1, sinkDelta),
+			sinkITCEventTree.call(this, tree2, sinkDelta),
+		];
+	};
+};
+
+function joinITCEventTrees(treeA, treeB) {
+	var treeAIsNumber = typeof treeA == 'number',
+	    treeBIsNumber = typeof treeB == 'number';
+
+	if (treeAIsNumber & treeBIsNumber) {
+		return Math.max(treeA, treeB);
+	} else if (treeAIsNumber) {
+		return joinITCEventTrees.call(this, [treeA, 0, 0], treeB);
+	} else if (treeBIsNumber) {
+		return joinITCEventTrees.call(this, treeA, [treeB, 0, 0]);
+	} else if (treeA[0] > treeB[0]) {
+		return joinITCEventTrees.call(this, treeB, treeA);
+	} else {
+		var treeA0 = treeA[0],
+		    treeB0 = treeB[0],
+		    liftDelta = treeB0 - treeA0;
+
+		return normITCEventTree.call(this, [treeA0,
+			joinITCEventTrees.call(this, treeA[1], liftITCEventTree.call(this, treeB[1], liftDelta)),
+			joinITCEventTrees.call(this, treeA[2], liftITCEventTree.call(this, treeB[2], liftDelta)),
+		]);
+	};
+};
+
+
+ITCEvent.join = function joinITCEvents(evA, evB) {
+	var treeA = evA ? evA.tree : 0,
+	    treeB = evB ? evB.tree : 0;
+	return new this(joinITCEventTrees.call(this, treeA, treeB));
+};
+
+
 if (typeof module == 'object') {
 	module.exports = ITCEvent;
 } else {
